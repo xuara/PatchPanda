10955b858d84f5ad48d5be43f0ac56167ed1e4a9
xuara <18605027+xuara@users.noreply.github.com>
2026-05-21 03:42:03 -0400

    data: generate ef core migration for security updates

--- PatchPanda.Web/Migrations/20260521072912_AddSecurityUpdateEntities.Designer.cs
+++ PatchPanda.Web/Migrations/20260521072912_AddSecurityUpdateEntities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PatchPanda.Web.Db;
 
@@ -10,9 +11,11 @@ using PatchPanda.Web.Db;
 namespace PatchPanda.Web.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20260521072912_AddSecurityUpdateEntities")]
+    partial class AddSecurityUpdateEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
---
+++ PatchPanda.Web/Migrations/20260521072912_AddSecurityUpdateEntities.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatchPanda.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSecurityUpdateEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AIBreaking",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "AISummary",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "Body",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "Breaking",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "DateDiscovered",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "Ignored",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "IsSuspectedMalicious",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "Notified",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "Prerelease",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "SecurityAnalysis",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "VersionNumber",
+                table: "AppVersions");
+
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "AppSettings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AIBreaking",
+                table: "AppVersions",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AISummary",
+                table: "AppVersions",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Body",
+                table: "AppVersions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Breaking",
+                table: "AppVersions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDiscovered",
+                table: "AppVersions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Ignored",
+                table: "AppVersions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSuspectedMalicious",
+                table: "AppVersions",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AppVersions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Notified",
+                table: "AppVersions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Prerelease",
+                table: "AppVersions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecurityAnalysis",
+                table: "AppVersions",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VersionNumber",
+                table: "AppVersions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Value",
+                table: "AppSettings",
+                type: "TEXT",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
